e253cc20115c171d84a1deb6856dc3b166653a3f
igorekbr <igorekb2004@gmail.com>
2025-06-01 13:46:19 +0200

    poprawka3

--- src/Quick.h
+++ src/Quick.h
@@ -1,20 +1,39 @@
-#ifndef _QUICK_H
-#define _QUICK_H
-
+#ifndef QUICK_H
+#define QUICK_H
 
 #include "SortTech.h"
-#include "SortError.h"
 #include "SortData.h"
 
-template<class T, template<typename...> class Container>
+template <class T, template <typename> class Container>
 class Quick : public SortTech<T, Container> {
-  public:
-    inline SortError sort(SortData<T, Container> & data) override;
+public:
+    inline void sort(SortData<T>& data) override {
+        quickSort(data, 0, data.size() - 1);
+    }
 
+private:
+    void quickSort(SortData<T>& data, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(data, low, high);
+            quickSort(data, low, pivotIndex - 1);
+            quickSort(data, pivotIndex + 1, high);
+        }
+    }
+
+    int partition(SortData<T>& data, int low, int high) {
+        T pivot = data[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; ++j) {
+            if (data[j] < pivot) {
+                ++i;
+                data.swap(i, j);
+            }
+        }
+
+        data.swap(i + 1, high);
+        return i + 1;
+    }
 };
-template<class T, template<typename...> class Container>
-inline SortError Quick<T, Container>::sort(SortData<T, Container> & data) {
-  return SE_SUCCESS;
-}
 
 #endif
